2a72d265649bdbf7787a1730c1a7230bec686c74
MQuaresma <miguelquaresma97@gmail.com>
2019-01-15 17:31:26 +0000

    Cleaner code and removed some stubs

--- TODO.md
+++ TODO.md
@@ -2,14 +2,15 @@
 ## Basics
 - [x] ls feature on filesystem
 - [ ] mkdir feature on filesystem
-- [ ] open command on filesystem
+- [x] open command on filesystem
 - [ ] alert via email/phone number
 - [ ] web interface to insert auth code
 - [ ] cat/read command
 - [ ] write command
 
 ## FIX
-- [ ] List links: `.` and `..`
+- [x] List links: `.` and `..`
+- [ ] Remove permission/access restrictions
 
 ## Additional features
 - [ ] Use cryptographically secure RNG
--- src/mellon.c
+++ src/mellon.c
@@ -4,10 +4,10 @@
  */
 #define FUSE_USE_VERSION 31
 
-#include<fuse.h>
+#include<fuse/fuse.h>
 #include<stdio.h>
 #include<stdlib.h>
-#include <errno.h>
+#include<errno.h>
 #include<unistd.h>
 #include<string.h>
 #include<fcntl.h>
@@ -28,18 +28,90 @@ int gen2FACode(){
 	}*/
 }
 
+/**
+ * Initial configurations for file system:
+ *      - disable caching of various inode related info
+ */
+static void *mellon_init(struct fuse_conn_info *conn, struct fuse_config *cfg){
+    cfg->use_ino = 1;
+    cfg->nullpath_ok = 1;
+    cfg->entry_timeout = 0;
+	cfg->attr_timeout = 0;
+	cfg->negative_timeout = 0;
+    return NULL;
+}
+
+
+/**
+ * Check for access permissions (set with umask(0) call in main)
+ */
+static int mellon_access(const char *path, int mask){
+    if(access(path, mask)==-1) 
+        return -errno;
+    else return 0;
+}
+
+/**
+ * Get file attributes for access control
+ */
+static int mellon_getattr(const char *path, struct stat *st, struct fuse_file_info *fi){
+    if(fi) //check if file is open
+        fstat(fi->fh, st);
+    else 
+        lstat(path, st);
+    return 0;
+}
+
+/**
+ * Make subdirectory
+ */
+static int mellon_mkdir(const char *dir, mode_t mode){
+    if(mkdir(dir, mode)==-1) 
+        return -errno;
+    else return 0;
+}
+
+/**
+ * Remove directory
+ */
+static int mellon_rmdir(const char *dir){
+    if(rmdir(dir)==-1) 
+        return -errno;
+    else return 0;
+}
+
+/**
+ * List files in directory
+ * FIX
+ */
+static int mellon_readdir(const char *path, void *buffer, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi, enum fuse_readdir_flags flags){
+    if(!strcmp(path, "/")){
+        filler(buffer, ".", NULL, 0, 0);
+        filler(buffer, "..", NULL, 0, 0);
+        filler(buffer, "dummy_file", NULL, 0, 0);
+        filler(buffer, "dummy_file_1", NULL, 0, 0);
+    }
+    return 0;
+}
+
+/*
+ * Called when opening a file for reading/writing/appending
+ */
 static int mellon_open(const char *file_name, struct fuse_file_info *fi){
-    int fa_code, fh;
+    char fa_code[5];
 
-    printf("Enter access code: \n");
-    scanf(" %d", &fa_code);
-    getchar();
+    puts("Enter access code: ");
+    fgets(fa_code, sizeof(fa_code), stdin);
 
-    if(fa_code == 1)
+    if(!strcmp(fa_code, "1\n"))
         return 0;
     return -errno;
 }
 
+/*
+ * Called for reading a file e.g: cat <file_name>
+ * FIX: remove stubs/dummy files
+ */
 static int mellon_read(const char *file_name, char *buf, size_t size, off_t offset, struct fuse_file_info *fi){
     char dummy_content[] = "hello, this is just some test content";
     int len = strlen(dummy_content);
@@ -53,55 +125,23 @@ static int mellon_read(const char *file_name, char *buf, size_t size, off_t offs
     
 }
 
-/**
- * List file attributes
- */
-static int mellon_getattr(const char *path, struct stat *st, struct fuse_file_info *fi){
-    st->st_uid = getuid();
-    st->st_gid = getgid();
-    st->st_atime = time(NULL);
-    st->st_mtime = time(NULL);
-
-    if(!strcmp(path, "/")){ //root directory relative to mount point
-        st->st_mode =  S_IFDIR | 0755;
-        st->st_nlink = 2;  //. and ..
-    }else{
-        st->st_mode = S_IFREG | 0644; 
-        st->st_size = 1024;
-        st->st_nlink = 1;
-    }
-    return 0;
-}
-
-/**
- * List files in directory
- */
-static int mellon_readdir(const char *path, void *buffer, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi, enum fuse_readdir_flags flags){
-    printf("Mellon: listing files\n");
-    if(!strcmp(path, "/")){
-        filler(buffer, ".", NULL, 0, 0);                    //FIX: offset
-        filler(buffer, "..", NULL, 0, 0);                    //FIX: offset
-        filler(buffer, "dummy_file", NULL, 0, 0);
-        filler(buffer, "dummy_file_1", NULL, 0, 0);
-    }
-    return 0;
-}
-
 
 static struct fuse_operations mellon_ops = {
-    .init = 0,                                      //called when mounting the filesystem
+    .init = mellon_init,                                      //called when mounting the filesystem
     .getattr = mellon_getattr,                  
+    .access = mellon_access,
     .readdir = mellon_readdir,                      //called when listing a directory
     .open = mellon_open,                            //called when opening a file
     .read = mellon_read
+    /*.write = mellon_write,
+    .create = mellon_create,
+    .rename = mellon_rename,
+    .chmod = mellon_cmod,
+    .chown = mellon_chown,*/
+
 };
 
 int main(int argc, char *argv[]){
-    struct fuse_args args;
-
-    if(fuse_opt_parse(NULL, NULL, NULL, NULL)==-1)
-        exit(1);
-
+    umask(0); //remove all restrictions
     fuse_main(argc, argv, &mellon_ops, NULL);
-
 }
